abc81e3a36c5ed68bd8820b26002776d87d3ca9e
Ye Lwin Htay <yelwin.r.k@gmail.com>
2022-12-22 11:18:36 -0800

    deleted comment

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,7 +5,6 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
-// comment
 // Get all spots
 router.get('/', async (req, res) => {
     const spots = await Spot.findAll()
