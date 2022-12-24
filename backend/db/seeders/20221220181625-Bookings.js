047a391391d99a51d3c4375c06b95cda78d6ac8f
Ye Lwin Htay <yelwin.r.k@gmail.com>
2022-12-23 16:52:47 -0800

    Get all of the current user's booking done, deploy and test on render

--- backend/db/seeders/20221220181625-Bookings.js
+++ backend/db/seeders/20221220181625-Bookings.js
@@ -24,20 +24,20 @@ module.exports = {
       {
         spotId: 2,
         userId: 4,
-        startDate: '2023-01-15',
-        endDate: '2023-02-16'
+        startDate: new Date('2023-01-15'),
+        endDate: new Date('2023-02-16')
       },
       {
         spotId: 3,
         userId: 5,
-        startDate: '2023-02-15',
-        endDate: '2023-03-10'
+        startDate: new Date('2023-02-15'),
+        endDate: new Date('2023-03-10')
       },
       {
         spotId: 1,
         userId: 6,
-        startDate: '2023-03-09',
-        endDate: '2023-03-30'
+        startDate: new Date('2023-03-09'),
+        endDate: new Date('2023-03-30')
       }
     ], {})
   },
---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { User, Spot, SpotImage, Review, ReviewImage, Booking, sequelize } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+// Get all of the Current User's Bookings
+
+router.get('/current', requireAuth, async (req, res) => {
+
+    const userId = req.user.id;
+    const bookings = await Booking.findAll({
+        where: {
+            userId: userId
+        },
+        include: {
+                model: Spot,
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                }
+            }
+    })
+    // console.log('bookings----->', bookings)
+    let bookingList = [];
+    for (let booking of bookings) {
+        bookingList.push(booking.toJSON())
+    }
+    // console.log('bookingList---------->', bookingList)
+    for (let booking of bookingList) {
+        // console.log('spot.id --------------->', booking.Spot.id) // 2
+        const spotId = booking.Spot.id
+        const spotImages = await SpotImage.findAll({
+            where: {
+                spotId: spotId
+            }
+        })
+        // console.log('spotImages --------->', spotImages)
+        let imageList = []
+        spotImages.forEach(img => {
+            imageList.push(img.toJSON())
+        })
+        // console.log('imageList ==========>', imageList)
+        imageList.forEach(img => {
+            if (img.preview === true) {
+                booking.Spot.previewImage = img.url
+            } else {
+                booking.Spot.previewImage = 'Preview unavailable'
+            }
+        })
+    }
+    // Error handling
+    if (!bookings) {
+        res.statusCode = 404
+        return res.json({
+            message: 'No bookings found',
+            statusCode: 404
+        })
+    }
+    return res.json({
+        Bookings: bookingList
+    })
+})
+
+
+module.exports = router;
